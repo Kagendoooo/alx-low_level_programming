fe1cd835018c9f9a46ee9229711e94e74dd8e29b
kagendoooo <kagendo@gmail.com>
2023-06-16 14:01:27 -0700

    Hexadecimal and lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Print all numbers base sixteen lowercase'
+ * Return: Always 0
+*/
+int main(void)
+{
+	int p;
+	int q;
+
+	for (p = 48; p <= 57; p++)
+	{
+		putchar(p);
+	}
+	for (q = 97; q = 102; q++)
+	{
+		putchar(q);
+	}
+	putchar('\n');
+	return (0);
+}
