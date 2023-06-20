b6f9b76020edec6bd97a19412e380a9db77aa9a5
Kagendoooo <kagendo@>
2023-06-20 22:52:10 +0300

    Print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+  * print_alphabet_x10 - 'Print lowercase ten times'
+*/
+
+
+void print_alphabet_x10(void)
+{
+	int a;
+	int b;
+
+	for (a = 1; a <= 10; a++)
+	{
+		for (b =  97; b <= 122; b++)
+		{
+			_putchar(b);
+		}
+		_putchar('\n');
+	}
+}
