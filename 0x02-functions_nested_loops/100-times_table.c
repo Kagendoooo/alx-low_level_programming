ee07ff98e1e7c1c1503ab056e961932f01ae9ab2
Kagendoooo <kagendo@>
2023-06-21 05:22:52 +0300

    Print times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,42 +7,45 @@
 
 void print_times_table(int n)
 {
-	int x, y, z;
+	int i, j, k;
 
 	if (n >= 0 && n <= 15)
 	{
-		for (x = 0; x <= n; x++)
+		for (i = 0; i <= n; i++)
 		{
-			_putchar('0');
+			for (j = 0; j <= n; j++)
+			{
+				k = j * i;
+				if (j == 0)
+				{
+					_putchar(k + '0');
+				}
+				else if (k < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				}
+				else if (k >= 10 && k < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+				else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+
+			}
 		}
-		for (y = 0; y <= n; y++)
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
-		z = x * y;
-		if (y == 0)
-		{
-			_putchar(z + '0');
-		}
-		if (z <= 99)
-		{
-			_putchar(' ');
-		}
-		if (z <= 9)
-		{
-			_putchar(' ');
-		}
-		if (z >= 100)
-		{
-			_putchar((z / 100) + '0');
-			_putchar(((z / 10) % 10) + '0');
-		}
-		else if (z <= 99 && z >= 10)
-		{
-			_putchar((z / 10) + '0');
-			_putchar((z % 10) + '0');
-		}
-	_putchar('\n');
 	}
 }
