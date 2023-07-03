83d754eaa94a882c9a5fb25dc39b2d1e9a7685fa
Kagendoooo <kagendo@>
2023-07-03 23:12:58 +0300

    Value of pointer to char

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+  * set_string - Set value of pointer to char
+  * @s: pointer to pointer
+  * @to: pointer setting to char
+*/
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
